a823b4248be85066e4fc305014816f28edff89af
Rainlonely <rainlonely@me.com>
2014-03-03 15:06:50 +0800

    Demo shop Created
    Created

---
+++ cache/_virtuemart/452d756fad74916c8af49b2b05dcb24a-cache-_virtuemart-b0f7a096960abeb293d2eb511315e1c5.php
@@ -0,0 +1,1 @@
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"output";s:0:"";s:6:"result";s:246:"<option   value="8">Eyeglasses</option><option   value="11">Kids' Styles</option><option   value="12">Men's Styles</option><option   value="1">Sport Glasses</option><option   value="6">Sunglass</option><option   value="10">Women's Styles</option>";}
---
+++ templates/gk_shop_and_buy/layouts/blocks/footer.php
@@ -0,0 +1,24 @@
+<?php
+
+// No direct access.
+defined('_JEXEC') or die;
+
+?>
+
+<footer id="gkFooter" class="gkPage">
+	<?php if($this->API->modules('footer_nav')) : ?>
+	<div id="gkFooterNav">
+		<jdoc:include type="modules" name="footer_nav" style="<?php echo $this->module_styles['footer_nav']; ?>" modnum="<?php echo $this->API->modules('footer_nav'); ?>" />
+	</div>
+	<?php endif; ?>
+	
+	<?php if($this->API->get('copyrights', '') !== '') : ?>
+	<p id="gkCopyrights"><?php echo $this->API->get('copyrights', ''); ?></p>
+	<?php else : ?>
+	<p id="gkCopyrights">Template Design &copy; <a href="//www.gavick.com" title="Joomla Templates">Joomla Templates</a> GavickPro. All rights reserved.</p>
+	<?php endif; ?>
+	
+	<?php if($this->API->get('footer_text', '') !== '') : ?>
+	<p id="gkFooterText"><?php echo $this->API->get('footer_text', ''); ?></p>
+	<?php endif; ?>
+</footer>
